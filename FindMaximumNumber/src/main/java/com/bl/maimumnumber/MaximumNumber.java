164ca0fdae3aa07f8fbd72e16c13eff060a30614
pranjalikawale <pranjalikawale1578@gmail.com>
2020-04-27 20:47:24 -1200

    REFACTORED: Remove extra space

--- FindMaximumNumber/src/main/java/com/bl/maimumnumber/MaximumNumber.java
+++ FindMaximumNumber/src/main/java/com/bl/maimumnumber/MaximumNumber.java
@@ -2,7 +2,6 @@ package com.bl.maimumnumber;
 
 public class MaximumNumber {
     public static Integer getMaximum(Integer a,Integer b,Integer c){
-        Integer max=a;
         if (a.compareTo(b)>0 && a.compareTo(c)>0 )
             return a;
         else if (b.compareTo(c)>0)
